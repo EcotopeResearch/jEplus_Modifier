8719cd9a85fa86cfb58c218f79d6bee90161c724
sspiel4 <sspiel4@gmail.com>
2019-08-12 09:05:40 -0700

    add structured starting_file.jep with comments to show where to coppy and past things

--- jEplus_Creator.py
+++ jEplus_Creator.py
@@ -1,35 +1,33 @@
-#!/usr/bin/env python
-# coding: utf-8
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 08:10:36 2019
+@author: Scott
+"""
 
-# In[1]:
-
-
+## IMPORT PACKAGES
 import pandas as pd
+
+# READ FILES
 read_file1=open('add_param.txt', 'r')
 read_file2=open('add_idref.txt', 'r')
+# OPEN WRITE FILES
 write_file1=open('jEplusLines_1.txt','w')
 write_file2=open('jEplusLines_2.txt','w')
 write_file3=open('jEplusLines_3.txt','w')
 
-
-# In[2]:
-
-
+# READ CSV INPUTS
 inputs = pd.read_csv('inputs.csv')
 lines1=read_file1.readlines()
 lines2=read_file2.readlines()
 
-
-# In[3]:
-
-
+## LOOP THROUGH TO WRITE FILES
 for j in range(0,len(inputs)):
     for i in range(0,len(lines1)):
         line1=lines1[i].replace('(Number)',str(j+1))
         line1=line1.replace('(Name)',inputs['Name'][j])
         line1=line1.replace('(Description_String)','Description')
         line1=line1.replace('(search_tag)',inputs['search_tag'][j])
-        line1=(j*'  '+line1)
+        #line1=(j*'  '+line1)
         
         write_file1.write(line1)
     
@@ -40,16 +38,11 @@ for j in range(0,len(inputs)):
     write_file3.write('       </object>\n')
     write_file3.write('       </void>\n')
 
-
-# In[4]:
-
-
+## CLOSE WRITE FILES
 write_file1.close()
 write_file2.close()
 write_file3.close()
 
-
-# In[ ]:
 
 
 
